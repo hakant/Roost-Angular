59520b03418d38a759858a82d1b85a44807b3a5c
hakant <hakantuncer@gmail.com>
2017-10-24 23:27:01 +0200

    Start moving jquery manipulations of the roost template into angular components

--- roost-template/js/app.js
+++ roost-template/js/app.js
@@ -205,49 +205,49 @@ $(document).ready(function () {
     /*------------------------------
         Silk Carousel
     -------------------------------*/
-    if($('.header__recommended')[0]) {
-        $('.header__recommended .listings-grid').slick({
-            speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            dotsClass: 'slick-dots slick-dots-light',
-            infinite: true,
-            responsive: [
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                    }
-                },
-                {
-                    breakpoint: 960,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 700,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        dots: true,
-                        arrows: false
-                    }
-                },
-                {
-                    breakpoint: 550,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        dots: true,
-                        arrows: false
-                    }
-                }
-            ]
-        });
-    }
+    // if($('.header__recommended')[0]) {
+    //     $('.header__recommended .listings-grid').slick({
+    //         speed: 300,
+    //         slidesToShow: 4,
+    //         slidesToScroll: 1,
+    //         dotsClass: 'slick-dots slick-dots-light',
+    //         infinite: true,
+    //         responsive: [
+    //             {
+    //                 breakpoint: 1200,
+    //                 settings: {
+    //                     slidesToShow: 3,
+    //                     slidesToScroll: 3,
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 960,
+    //                 settings: {
+    //                     slidesToShow: 2,
+    //                     slidesToScroll: 2
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 700,
+    //                 settings: {
+    //                     slidesToShow: 2,
+    //                     slidesToScroll: 2,
+    //                     dots: true,
+    //                     arrows: false
+    //                 }
+    //             },
+    //             {
+    //                 breakpoint: 550,
+    //                 settings: {
+    //                     slidesToShow: 1,
+    //                     slidesToScroll: 1,
+    //                     dots: true,
+    //                     arrows: false
+    //                 }
+    //             }
+    //         ]
+    //     });
+    // }
 
     if($('.grid-widget--listings')[0]) {
         $('.grid-widget--listings').slick({
--- src/app/header/header-recommended/header-recommended.component.ts
+++ src/app/header/header-recommended/header-recommended.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PropertyService } from '../../services/property.service';
 import { Property } from '../../models/property';
 
+declare let $: any;
+
 @Component({
   selector: 'app-header-recommended',
   templateUrl: './header-recommended.component.html',
@@ -17,6 +19,53 @@ export class HeaderRecommendedComponent implements OnInit {
     this.propertyService.requestProperties().subscribe(
       data => {
         this.properties = data.list;
+        // @hakant: Can you find a better way for doing this?
+        // setTimeout makes sure the enableSlick function runs after the view is rendered
+        setTimeout(this.enableSlick, 0);
       });
+  }
+
+  enableSlick() {
+    $('.header__recommended .listings-grid').slick({
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      dotsClass: 'slick-dots slick-dots-light',
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          }
+        },
+        {
+          breakpoint: 960,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            dots: true,
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 550,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false
+          }
+        }
+      ]
+    });
   }
 }
